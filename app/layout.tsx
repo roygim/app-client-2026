8536ab242a84ed126b1da125f5b29d3c71f5544b
roygim <roygim@gmail.com>
2026-06-12 18:00:37 +0300

    roeig

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
-          <div className="bg-background-primary">
+          <div className="bg-background-primary text-foreground min-h-screen">
             <header className="max-w-7xl mx-auto px-2 sm:px-0">
               <Header />
             </header>
